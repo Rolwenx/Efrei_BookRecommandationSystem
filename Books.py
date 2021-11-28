d016e95cac78b121ff2eb5e4644fc04b4c06af83
Rolwenx <n.rnolwen@gmail.com>
2021-11-28 02:54:16 +0100

    delete add_reader.py and Books.py for now

--- Books.py
+++
@@ -1,9 +0,0 @@
-#Projet Python Yoke Joseph BOOKS
-def list_books():
-    """Prints the list of the books preceded by the sequential number"""
-    file_books_list = open("books.txt", "r")
-    a = 1
-    for line in file_books_list:
-        print(a,') ',line,end='')
-        a+=1
-    file_books_list.close()
